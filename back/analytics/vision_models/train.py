a4f8e7915977cbe3e502b22f82f683d659085f1f
lizkartoffel <witchylizzie100@gmail.com>
2025-12-05 22:07:03 +0300

    fooooooooooood

--- back/analytics/vision_models/train.py
+++ back/analytics/vision_models/train.py
@@ -19,7 +19,7 @@ DATASET_DIR = os.path.join(
 # --- CONFIG ---
 IMG_SIZE = 224
 BATCH_SIZE = 32
-EPOCHS = 30
+EPOCHS = 50
 NUM_CLASSES = 8
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using device:", DEVICE)
